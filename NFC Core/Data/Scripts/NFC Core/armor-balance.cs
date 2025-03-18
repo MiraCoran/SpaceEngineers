71a63213639a9474220869f7b041f8b3fd77fe59
Mira Coran <miracoran@gmail.com>
2025-03-18 14:22:09 -0700

    initial commit

---
+++ NFC Core/Data/Scripts/NFC Core/armor-balance.cs
@@ -0,0 +1,45 @@
+﻿// armor-balance.cs
+// copyright (c) 2025 by jay baxter (jaybaxter@me.com)
+
+using Sandbox.Definitions;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+
+[MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+public class ArmorBalance : MySessionComponentBase {
+	public override void Init(MyObjectBuilder_SessionComponent unused) { }
+
+	public override void LoadData() {
+		modifyArmorResists();
+	}
+
+	private void modifyArmorResists()
+	{
+		const float armorDamageResist = 0.20f; // 25% damage resistance
+		const float armorDeformReduction = 0.20f; // 25% less deformation
+
+		try {
+			foreach (var def in MyDefinitionManager.Static.GetAllDefinitions()) {
+				var blockDef = def as MyCubeBlockDefinition;
+
+				// try to exclude non-armor blocks
+				if (blockDef == null) continue;
+				//if (blockDef.BlockTopology == null) continue;
+				if (blockDef.BlockTopology == MyBlockTopology.TriangleMesh) continue;
+				if (blockDef.CubeDefinition == null) continue;
+				if (!blockDef.CubeDefinition.ShowEdges) continue;
+
+				blockDef.GeneralDamageMultiplier = 1 - armorDamageResist;
+				blockDef.DeformationRatio = 1 - armorDeformReduction;
+			}
+		}
+		catch (System.Exception ex) {
+			using (var writer =
+			       MyAPIGateway.Utilities.WriteFileInWorldStorage(
+				       file: "armor-balance.log",
+				       callingType: typeof(ArmorBalance))) {
+				writer.WriteLineAsync(ex.ToString());
+		}
+	}
+}
---
+++ NFC Core/README.md
@@ -0,0 +1,4 @@
+# NFC Core
+A Space Engineers conversion mod.
+
+This work is licensed under CC BY-NC-SA 4.0. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/.
